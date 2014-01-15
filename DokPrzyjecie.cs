a4c5c7934e0873f23ca13e3ca74209131aa324d0
bkozak <bartek.kozak17@gmail.com>
2014-01-15 09:37:38 +0100

    Create DokPrzyjecie.cs

---
+++ DokPrzyjecie.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ObslugaMagazynuLib.Dokumenty
+{
+    public class DokPrzyjecie : Document
+    {
+        public override string Type { get { return "PT"; } }
+        public override bool Execute()
+        {
+            //dodanie dokumentu do ewidencji dokumentow w bazie
+            this.AddNewItem(_elements._towarList.Count > 0 ? true : false);
+
+            MySQL sql = MySQL.Instance;
+            foreach (Towary.TowarAbstract t in _elements._towarList)
+            {
+                //zapytanie do sprawdzenia
+                int? r = sql.Query("UPDATE towary SET towar_stan = towar_stan + " + t.Stan + " WHERE towar_id = " + t.Id);
+                if (r == null)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
